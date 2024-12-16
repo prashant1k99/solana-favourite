bc6701dd41133acccfcb1c353427ebc26db92224
Prashant <prashantco111@gmail.com>
2024-12-16 21:43:36 +0530

    Project Init

---
+++ programs/favourites/src/lib.rs
@@ -0,0 +1,11 @@
+use anchor_lang::prelude::*;
+
+declare_id!("J17SgfdpURh63ScmEXJ6u9hC94rqSj7gBT96KT1boDSb");
+
+#[program]
+pub mod favourites {
+    use super::*;
+}
+
+#[derive(Accounts)]
+pub struct Favourites {}
---
+++ tests/favourites.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import * as favourites from "../target/types/favourites";
+
+describe("favourites", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Favourites as Program<favourites.Favourites>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
